bc7b5d36e6beda8f972fa7d4a396e93981db27ce
wade <wade.fs@gmail.com>
2026-05-27 08:39:13 +0800

    try to fix sshd

--- mudlib/std/guest.c
+++ mudlib/std/guest.c
@@ -27,9 +27,6 @@ void set_temp(string key, mixed val) {
 void catch_tell(string msg) {
     if (!msg || msg == "") return;
     
-    // 除錯日誌：查看是否有訊息進入 catch_tell
-    printf("DEBUG [Guest]: Received msg: %s\n", msg);
-    
     if (!session_id || !remote_mudlib) return;
 
     object ssh_d = load_object("/secure/ssh_d.c");
